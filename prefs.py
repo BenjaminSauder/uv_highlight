8907d732e586541d7da6932b9499e00541884964
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2019-07-20 01:16:14 +0200

    * switched to properties

--- prefs.py
+++ prefs.py
@@ -6,45 +6,45 @@ debug = True
 class Addon(bpy.types.AddonPreferences):
     bl_idname = __package__
 
-    max_verts = bpy.props.IntProperty("Max Verts", default=50000)
+    max_verts : bpy.props.IntProperty("Max Verts", default=50000)
 
-    view3d_selection_verts_edges = bpy.props.FloatVectorProperty(name="view3d_selection_verts_edges",
+    view3d_selection_verts_edges : bpy.props.FloatVectorProperty(name="view3d_selection_verts_edges",
                                                                  subtype="COLOR",
                                                                  default=(
                                                                      0, 1.0, 1.0, 1.0),
                                                                  size=4)
 
-    view3d_selection_faces = bpy.props.FloatVectorProperty(name="view3d_selection_faces",
+    view3d_selection_faces : bpy.props.FloatVectorProperty(name="view3d_selection_faces",
                                                            subtype="COLOR",
                                                            default=(
                                                                0, 1.0, 1.0, 0.4),
                                                            size=4)
 
-    view3d_preselection_verts_edges = bpy.props.FloatVectorProperty(name="view3d_preselection_verts_edges ",
+    view3d_preselection_verts_edges : bpy.props.FloatVectorProperty(name="view3d_preselection_verts_edges ",
                                                                     subtype="COLOR",
                                                                     default=(
                                                                         1.0, 0.0, 0.0, 1.0),
                                                                     size=4)
 
-    view3d_preselection_faces = bpy.props.FloatVectorProperty(name="view3d_preselection_faces",
+    view3d_preselection_faces : bpy.props.FloatVectorProperty(name="view3d_preselection_faces",
                                                               subtype="COLOR",
                                                               default=(
                                                                   0.15, 0.15, 0.15, 1.0),
                                                               size=4)
 
-    uv_preselection_verts_edges = bpy.props.FloatVectorProperty(name="uv_preselection_verts_edges",
+    uv_preselection_verts_edges : bpy.props.FloatVectorProperty(name="uv_preselection_verts_edges",
                                                                 subtype="COLOR",
                                                                 default=(
                                                                     1.0, 1.0, 1.0, 1.0),
                                                                 size=4)
 
-    uv_preselection_faces = bpy.props.FloatVectorProperty(name="uv_preselection_faces",
+    uv_preselection_faces : bpy.props.FloatVectorProperty(name="uv_preselection_faces",
                                                           subtype="COLOR",
                                                           default=(
                                                               0.15, 0.15, 0.15, 1.0),
                                                           size=4)
 
-    uv_hidden_faces = bpy.props.FloatVectorProperty(name="uv_hidden_faces",
+    uv_hidden_faces : bpy.props.FloatVectorProperty(name="uv_hidden_faces",
                                                     subtype="COLOR",
                                                     default=(
                                                         0.15, 0.15, 0.15, 1.0),
