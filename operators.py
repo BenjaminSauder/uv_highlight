9bd7c6ebcdc81b111b2fcc75cae31fefbf89b44c
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2019-07-25 00:15:22 +0200

    * cleanup

--- operators.py
+++ operators.py
@@ -9,12 +9,12 @@ from .prefs import debug
 class UV_OT_Timer(bpy.types.Operator):
     '''
     Heartbeat of the uv updater.
-    Basically polls the mouse position and invokes the uv updates.    
+    Basically polls the mouse position and invokes the update handler.    
     '''
 
     bl_idname = "uv.timer"
     bl_label = "UV Timer"
-    bl_options = {"REGISTER"}  # , "INTERNAL"}
+    bl_options = {"REGISTER", "INTERNAL"}
 
     _timer = None
 
@@ -60,30 +60,9 @@ class UV_OT_Timer(bpy.types.Operator):
                     mouse_region_x < region_x + width and
                         mouse_region_y < region_y + height):
                     p = mathutils.Vector(region_to_view(
-                        mouse_region_x, mouse_region_y))
-                   
+                        mouse_region_x, mouse_region_y))                   
                     main.updater.mouse_position = p.resized(3)
 
     def cancel(self, context):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
-
-
-# class UV_OT_mouseposition(bpy.types.Operator):
-#     """ This operator grabs the mouse location """
-#     bl_idname = "uv.mouseposition"
-#     bl_label = "UV Mouse location"
-#     # bl_options = {"REGISTER"}#, "INTERNAL"}
-
-#     def invoke(self, context, event):
-#         #print( event.type )
-#         if event.type == 'MOUSEMOVE':
-#             main.UV_MOUSE = None
-
-
-#         return {'FINISHED'}
-
-#     # def invoke(self, context, event):
-#     #     self.mousepos = (0, 0)
-#     #     context.window_manager.modal_handler_add(self)
-#     #     return {'RUNNING_MODAL'}
