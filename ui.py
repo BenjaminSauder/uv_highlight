5a30f8cd64129a68148a2d61f9b9d990f3805d09
Benjamin Sauder <13512160+BenjaminSauder@users.noreply.github.com>
2019-09-01 21:20:03 +0200

    * rearrange UI

--- ui.py
+++ ui.py
@@ -3,13 +3,14 @@ import bpy
 class IMAGE_PT_uv_highlight(bpy.types.Panel):
     bl_space_type = 'IMAGE_EDITOR'
     bl_region_type = 'UI'
+    bl_parent_id = 'IMAGE_PT_view_display'
     bl_category = "View"
     bl_label = "UV Highlight"
 
     @classmethod
     def poll(cls, context):
         sima = context.space_data
-        return sima.show_uvedit
+        return (sima and (sima.show_uvedit))
 
     def draw(self, context):
         layout = self.layout
